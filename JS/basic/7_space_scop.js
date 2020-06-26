b3de373e7d5dec7387306aa79052cbd2e110f8a1
wxqSnowing <707409166@qq.com>
2020-06-26 14:57:11 +0800

    update

---
+++ JS/basic/7_space_scop.js
@@ -0,0 +1,16 @@
+console.log(test);
+
+function test() {
+    var a = 1;
+}
+
+console.log(test2);
+
+var test2 = function() {
+    var b = 2;
+}
+
+console.log(test3);
+var test3 = () => {
+    var c = 3;
+}
