6c5f866614ea00c87d35b38e41fbde08e4a15280
wxqSnowing <707409166@qq.com>
2020-07-08 19:41:11 +0800

    update

--- React/Demo/demo_father_child/src/index.jsx
+++ React/Demo/demo_father_child/src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import FatherChild from './pages/FatherChild';
-import UserListConteiner from './pages/UserListConteiner';
+// import UserListConteiner from './pages/UserListConteiner';
+import MyHook from './pages/MyHook'
 
 ReactDOM.render(
   <React.StrictMode>
       {/* <FatherChild></FatherChild> */}
-      <UserListConteiner></UserListConteiner>
+      {/* <UserListConteiner></UserListConteiner> */}
+      <MyHook></MyHook>
   </React.StrictMode>,
   document.getElementById('root')
 );
