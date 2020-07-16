84360be1b29d61274466be2ffa193c5639a91c36
wxqSnowing <707409166@qq.com>
2020-07-16 15:22:12 +0800

    update

---
+++ 算法/15. 未实现_合并两个有序数列.js
@@ -0,0 +1,8 @@
+function mergeArr(arr1, arr2) {
+    return arr1.concat(arr2).sort();
+}
+console.log(mergeArr([1, 2, 3, 4], [2, 3, 5]));
+console.log(JSON.stringify([
+    [1, 2, 3],
+    [2, 3, 4]
+]));
